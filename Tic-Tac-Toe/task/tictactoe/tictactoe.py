6831335c41a4c6c10a415dde29d528756dce77ca
GeekvsCritic <saichaitanya.408@user.github.com>
2020-06-21 12:02:19 +0530

    Stage2

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,1 +1,20 @@
 # write your code here
+class TicTacToe:
+    input_values = input()
+
+    def standard_matrix(self):
+        counter = 0
+        temp_list = []
+        print("---------")
+        #  print("X O X \nO X X \nX X O")
+        for value in self.input_values:
+            counter += 1
+            temp_list += value
+            if counter % 3 == 0:
+                print(f"| {temp_list[0]} {temp_list[1]} {temp_list[2]} |")
+                temp_list = []
+        print("---------")
+
+
+game = TicTacToe()
+game.standard_matrix()
